b8f80d95a195b0fd31048f3777c532c9539d4184
Abdullahi Halimat <halimatabdullahi86@gmail.com>
2022-04-08 10:50:44 -0700

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main- Entry point
+*       Prints a random number and states whether
+*        it is positive, negative, or zero.
+* Return: Always 0.
+*/
+int main(void)
+{
