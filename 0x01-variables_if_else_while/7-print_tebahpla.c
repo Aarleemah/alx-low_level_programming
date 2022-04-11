ac44f2fc344522d463ad24917f7ce8f8d583c53f
Abdullahi Halimat <halimatabdullahi86@gmail.com>
2022-04-11 12:19:01 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints the letters of the alphabet.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char Revalpha[26] = "zyxwvutsrqponmlkjhgfedcba";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(Revalpha[i]);
+}
+putchar('\n');
+return (0);
+}
