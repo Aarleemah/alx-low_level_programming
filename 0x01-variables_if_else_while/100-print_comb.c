07cb76465c6af3365dc546707c7dbdc27a84d9c2
Abdullahi Halimat <halimatabdullahi86@gmail.com>
2022-04-11 12:43:46 -0700

    advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - print double digit combos
+*
+* Description: print double digit combos
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int j, k;
+
+j=48;
+k = 48;
+
+while (i < 58)
+{
+k = j + 1;
+while (k < 58)
+{
+putchar(j);
+putchar(k);
+
+if (j < 56 || k < 57)
+{
+putchar(44);
+putchar(32);
+}
+k++;
+}
+
+j++;
+}
+
+putchar(10);
+
+return (0);
+}
