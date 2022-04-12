dbfebe1e2b7d7f15eb29ced72fb33f24443f737e
Abdullahi Halimat <halimatabdullahi86@gmail.com>
2022-04-12 10:52:37 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,9 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabet in lowercase, followed by a new line.
+* main - main block
+root@e0a4fdf416a2:/alx-low_level_programming/0x02-functions_nested_loops# cat > 1-alphabet.c
 #include main.h
 
 /**
