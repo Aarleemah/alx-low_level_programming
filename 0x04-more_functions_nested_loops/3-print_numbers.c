280b1ce3da3ba804e85bb4001cc0bdb0feefee1c
Abdullahi halimat <halimatabdullahi86@gmail.com>
2022-04-14 14:44:17 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,6 +4,14 @@
 * print_numbers - prints the number zero to nine
 *
 * Return: Always 0
+8
+root@e0a4fdf416a2:/alx-low_level_programming/0x04-more_functions_nested_loops# cat > 3-print_numbers.c
+#include "main.h"
+
+/**
+* print_numbers - prints the number zero to nine
+*
+* Return: Always 0
 */
 
 void print_numbers(void)
@@ -11,9 +19,9 @@ void print_numbers(void)
 
 int i;
 
-i=0;
-
 for (i = 0; i < 10; i++)
+{
 _putchar(i + '0');
-_putchar ('\n');
+}
+_putchar('\n');
 }
